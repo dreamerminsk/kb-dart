db8248af2402a4ec4d3d2fba343c3f3a05fdc46c
Micah Teeuws <tutmirage@gmail.com>
2022-02-17 14:39:30 +0300

    Create kinobusiness.dart

---
+++ lib/src/kb/kinobusiness.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import 'dart:developer' as developer;
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:html/dom.dart' as dom;
+import 'package:html/parser.dart';
+
+class KinoBusiness {
+  
+  
+}
