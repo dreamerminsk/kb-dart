a32a6e07865e151aa6da5dbba7853fa75ab23680
Micah Teeuws <tutmirage@gmail.com>
2022-02-17 14:44:09 +0300

    Update kinobusiness.dart

--- lib/src/kb/kinobusiness.dart
+++ lib/src/kb/kinobusiness.dart
@@ -8,5 +8,14 @@ import 'package:html/parser.dart';
 
 class KinoBusiness {
   
+  static final kbHost = 'https://kinobusiness.com';
+
+  static final yearBoxOffice = '$kbHost/kassovye_sbory/films_year/';
+
+  static final weekendBoxOffice = '$kbHost/kassovye_sbory/weekend/';
+
+  static final thursdayBoxOffice = '$kbHost/kassovye_sbory/thursday/';
+  
+  static final dayBoxOffice = '$kbHost/kassovye_sbory/day/';  
   
 }
