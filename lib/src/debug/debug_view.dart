3999dc9fe99aa8ae6c72aeca7a9aaff4c33cd87a
Micah Teeuws <tutmirage@gmail.com>
2024-02-12 20:23:31 +0300

    Update debug_view.dart

--- lib/src/debug/debug_view.dart
+++ lib/src/debug/debug_view.dart
@@ -6,7 +6,7 @@ import 'files/root_paths.dart';
 import 'scanner/scanner_widget.dart';
 import 'widgets/health_widget.dart';
 import 'widgets/network_stats.dart';
-import 'widgets/random_json_files.dart';
+import 'json/random_json_files.dart';
 import 'theme/color_scheme_widget.dart';
 import 'theme/text_theme_widget.dart';
 import 'theme/theme_data_widget.dart';
