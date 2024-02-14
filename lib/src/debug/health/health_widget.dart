c260702d4b6978fa4f58c5b95fd4532a132a2138
Micah Teeuws <tutmirage@gmail.com>
2024-02-14 15:20:32 +0300

    Update debug

---
+++ lib/src/debug/health/health_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+//import 'package:intl/intl.dart';
+
+class HealthWidget extends StatelessWidget {
+  final Map<String, int> controllers;
+
+  const HealthWidget({
+    super.key,
+    required this.controllers,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      width: Get.width,
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: _buildList(context, controllers),
+          ), // Column
+          RotatedBox(
+            quarterTurns: 3,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(0, 8, 0, 4),
+              child: Text(
+                'Health Status',
+                style: textTheme.titleLarge!
+                    .copyWith(color: colorScheme.onPrimary),
+              ), // Text
+            ), // Padding
+          ), // RotatedBox
+        ],
+      ), // Row
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ), // BorderRadius
+        color: colorScheme.primary,
+      ), // BoxDecoration
+    ); //Container
+  }
+
+  List<Widget> _buildList(BuildContext context, Map<String, int> items) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return items.entries
+        .take(7)
+        .map<Widget>((item) => Text(
+              '${item.key}: ${item.value}',
+              style:
+                  textTheme.bodyMedium!.copyWith(color: colorScheme.onPrimary),
+            ))
+        .toList();
+  }
+}
