105b42c9f3bc2cb23070a9629913ba163e8c077a
Micah Teeuws <tutmirage@gmail.com>
2024-06-29 08:25:01 +0300

    Update category_view.dart

--- lib/src/category/category_view.dart
+++ lib/src/category/category_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'category_controller.dart';
 import '../wiki/entities/category_info.dart';
 import '../wiki/entities/category_member.dart';
@@ -10,7 +9,7 @@ class CategoryView extends StatelessWidget {
   final CategoryController c = Get.find();
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
@@ -18,44 +17,54 @@ class CategoryView extends StatelessWidget {
       body: SafeArea(
         child: Stack(
           children: <Widget>[
-            Container(
-              height: double.infinity,
-              width: double.infinity,
-              padding: const EdgeInsets.all(8),
-              color: colorScheme.primary,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(height: 8),
-                  Obx(() => Text(
-                        c.category.value?.title ?? '~~~',
-                        style: textTheme.headlineSmall!
-                            .copyWith(color: colorScheme.onPrimary),
-                      )), // Text
-                  SizedBox(height: 12),
-                  Obx(() => _buildProps(context, c.category.value)),
-                ],
-              ), // Column
-            ), // Container
-            Positioned(
-              top: 150,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                child: Obx(() => _buildList(context, c.members)),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ), // BorderRadius
-                  color: colorScheme.secondaryContainer,
-                ), // BoxDecoration
-              ), //Container
-            ), // Positioned
+            _buildBackground(colorScheme),
+            _buildForeground(context, textTheme, colorScheme),
           ],
-        ), //Stack
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackground(ColorScheme colorScheme) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      padding: const EdgeInsets.all(8),
+      color: colorScheme.primary,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 8),
+          Obx(() => Text(
+                c.category.value?.title ?? '~~~',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: colorScheme.onPrimary,
+                ),
+              )),
+          SizedBox(height: 12),
+          Obx(() => _buildProps(context, c.category.value)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildForeground(BuildContext context, TextTheme textTheme, ColorScheme colorScheme) {
+    return Positioned(
+      top: 150,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          color: colorScheme.secondaryContainer,
+        ),
+        child: Obx(() => _buildList(context, c.members)),
       ),
     );
   }
@@ -66,124 +75,66 @@ class CategoryView extends StatelessWidget {
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(4.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            border: Border.all(
-              width: 1.0,
-              color: colorScheme.onPrimary,
-            ),
-          ),
-          child: Column(
-            children: <Widget>[
-              Text(
-                ci?.subcats.toString() ?? '∞',
-                style: textTheme.titleLarge!
-                    .copyWith(color: colorScheme.onPrimary),
-              ),
-              Text(
-                'subcats',
-                style:
-                    textTheme.bodyLarge!.copyWith(color: colorScheme.onPrimary),
-              ),
-            ],
-          ), // Column
-        ), // Container
+        _buildPropItem(context, textTheme, colorScheme, ci?.subcats.toString() ?? '∞', 'subcats'),
+        _buildPropItem(context, textTheme, colorScheme, ci?.pages.toString() ?? '∞', 'pages'),
+        _buildPropItem(context, textTheme, colorScheme, '∞', 'cats'),
+        _buildPropItem(context, textTheme, colorScheme, '∞', 'langs'),
+      ],
+    );
+  }
 
-        Column(
-          children: <Widget>[
-            Text(
-              ci?.pages.toString() ?? '∞',
-              style:
-                  textTheme.titleLarge!.copyWith(color: colorScheme.onPrimary),
-            ),
-            Text(
-              'pages',
-              style:
-                  textTheme.bodyLarge!.copyWith(color: colorScheme.onPrimary),
-            ),
-          ],
+  Widget _buildPropItem(BuildContext context, TextTheme textTheme, ColorScheme colorScheme, String value, String label) {
+    return Column(
+      children: <Widget>[
+        Text(
+          value,
+          style: textTheme.titleLarge!.copyWith(color: colorScheme.onPrimary),
         ),
-        Column(
-          children: <Widget>[
-            Text(
-              '∞',
-              style:
-                  textTheme.titleLarge!.copyWith(color: colorScheme.onPrimary),
-            ),
-            Text(
-              'cats',
-              style:
-                  textTheme.bodyLarge!.copyWith(color: colorScheme.onPrimary),
-            ),
-          ],
-        ),
-        Column(
-          children: <Widget>[
-            Text(
-              '∞',
-              style:
-                  textTheme.titleLarge!.copyWith(color: colorScheme.onPrimary),
-            ),
-            Text(
-              'langs',
-              style:
-                  textTheme.bodyLarge!.copyWith(color: colorScheme.onPrimary),
-            ),
-          ],
+        Text(
+          label,
+          style: textTheme.bodyLarge!.copyWith(color: colorScheme.onPrimary),
         ),
       ],
     );
   }
 
   Widget _buildList(BuildContext context, List<CategoryMember> query) {
-    //final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
-    return query.length > 0
+    return query.isNotEmpty
         ? ListView.builder(
             itemCount: query.length,
             itemBuilder: (BuildContext context, int index) {
-              return InkWell(
-                  onTap: () {}, child: _catCard(context, query[index]));
+              return _catCard(context, query[index]);
             },
           )
         : Center(
-            child:
-                Icon(Icons.category, color: colorScheme.primary, size: 128.0),
+            child: Icon(Icons.category, color: colorScheme.primary, size: 128.0),
           );
   }
-
-  //Widget _buildCard(BuildContext context, CategoryMember cm) {
-  //  final textTheme = Theme.of(context).textTheme;
-  //  final colorScheme = Theme.of(context).colorScheme;
-
-  //  return Text('$cm',
-  //    style: textTheme.bodyLarge!
-  //      .copyWith(
-  //        color: colorScheme.onSecondaryContainer
-  //      ),
-  //  );
-  //}
 
   Widget _catCard(BuildContext context, CategoryMember cm) {
     return InkWell(
       onTap: () {
-        Get.toNamed(Routes.CATEGORY,
-            arguments: CategoryInfo(
-                title: cm.title, lang: c.category.value?.lang ?? 'en'),
-            preventDuplicates: false);
+        Get.toNamed(
+          Routes.CATEGORY,
+          arguments: CategoryInfo(
+            title: cm.title,
+            lang: c.category.value?.lang ?? 'en',
+          ),
+          preventDuplicates: false,
+        );
       },
       child: Card(
         child: ListTile(
-          leading: Icon(Icons.category,
-              color: Theme.of(context).colorScheme.primary, size: 64.0),
-          title: Text('${cm.title}'),
-          subtitle: Text('${cm.timestamp}'),
-          //subtitle: Text('${cat.subcats} categories, ${cat.pages} pages')
+          leading: Icon(
+            Icons.category,
+            color: Theme.of(context).colorScheme.primary,
+            size: 64.0,
+          ),
+          title: Text(cm.title),
+          subtitle: Text(cm.timestamp),
         ),
       ),
     );
