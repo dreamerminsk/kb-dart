5847d44aa626cf0721d258d94a26fb89464e4194
Micah Teeuws <tutmirage@gmail.com>
2025-09-05 20:57:07 +0300

    Update home_controller.dart

--- lib/src/home/home_controller.dart
+++ lib/src/home/home_controller.dart
@@ -107,7 +107,7 @@ class HomeController extends GetxController {
       }
       if (zeroes[0].wiki != null &&  zeroes[0].wiki!.title != null) {
         final sum = await readSummary(zeroes[0].wiki!.title!);
-        zeroes[0].wiki!.description = sum.description;
+        zeroes[0].wiki!.description = sum.extract;
       }
       animeList.sort(
           (a, b) => (b.wiki?.mviMonth ?? 0).compareTo(a.wiki?.mviMonth ?? 0));
