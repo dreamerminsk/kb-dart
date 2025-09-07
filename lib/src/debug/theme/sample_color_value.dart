066bdc49ba177722682060357dad1cbb49e25180
Micah Teeuws <tutmirage@gmail.com>
2025-09-07 19:00:20 +0300

    Update sample_color_value.dart

--- lib/src/debug/theme/sample_color_value.dart
+++ lib/src/debug/theme/sample_color_value.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../core/color_utils.dart';
+import 'chessboard.dart';
 
 class SampleColorValue extends StatelessWidget {
   final Color value;
@@ -15,54 +16,59 @@ class SampleColorValue extends StatelessWidget {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-    final containerHeight = Get.width / 2 / 1.618;
+final containerHeight = Get.width / 2 / 1.618;
     final containerColor =
-        Color.fromARGB(255, value.red, value.green, value.blue);
+        Color.fromARGB(value.alpha, value.red, value.green, value.blue);
 
     return Material(
       elevation: 4,
       borderRadius: BorderRadius.circular(20),
-      child: Container(
-        width: double.infinity,
-        height: Get.width / 2 / 1.618,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: containerColor,
-        ),
-        padding: EdgeInsets.all(16.0), // Увеличенные отступы
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            AspectRatio(
-              aspectRatio: 1.0,
-              child: Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: colorScheme.primary,
-                ),
-                child: Text(
-                  'C',
-                  style: Theme.of(context)
-                      .textTheme
-                      .displayLarge!
-                      .copyWith(color: colorScheme.onPrimary),
-                ),
-              ),
+      child: Stack(
+        children: [
+          ChessboardBackground(squareSize: Get.width / 2 / 1.618),
+          Container(
+            width: double.infinity,
+            height: containerHeight,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: containerColor,
             ),
-            SizedBox(width: 16), // Отступ между элементами
-            Container(
-              alignment: Alignment.center,
-              color: Colors.transparent,
-              child: Text(
-                '#${value.value.toRadixString(16).padLeft(8, '0').toUpperCase()}',
-                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: colorScheme.primary,
+                    ),
+                    child: Text(
+                      'C',
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayLarge!
+                          .copyWith(color: colorScheme.onPrimary),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 16),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    '#${alpha.toRadixString(16).padLeft(2, '0').toUpperCase()}',
+                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(
                     color: ColorUtils.contrastThemeColor(containerColor)),
-              ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
+
