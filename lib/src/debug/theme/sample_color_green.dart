67e5669503e6ee2ba6151c14540dee44f85d557d
Micah Teeuws <tutmirage@gmail.com>
2025-09-07 23:33:57 +0300

    Update sample_color_green.dart

--- lib/src/debug/theme/sample_color_green.dart
+++ lib/src/debug/theme/sample_color_green.dart
@@ -13,7 +13,7 @@ class SampleColorGreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
+    //final colorScheme = Theme.of(context).colorScheme;
 
     final containerHeight = Get.width / 2 / 1.618;
     final containerColor = Color.fromARGB(255, 0, green, 0);
